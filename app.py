cdde828dd2cb2fc18c226d2a47e6f716307a3d2d
itochax23 <86204563+itochax23@users.noreply.github.com>
2021-08-29 22:01:07 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,36 @@
 # import dependencies
-from flask import Flask
+import datetime as dt
+import numpy as np
+import pandas as pd
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from flask import Flask, jsonify
 
-# create a new Flask app instance
+# Create engine, reflect database
+
+engine = create_engine("sqlite:///hawaii.sqlite")
+Base = automap_base() 
+Base.prepare(engine, reflect=True)
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+# Create session link
+session = Session(engine)
+
+# Create a new Flask app instance
 app = Flask(__name__)
 
-# create our first route, or root
+# create our welcome route
 @app.route('/')
-def hello_world():
-    return 'Hello world'
-
-
+def welcome():
+    return(
+    '''
+    Welcome to the Climate Analysis API!
+    Available Routes:
+    /api/v1.0/precipitation
+    /api/v1.0/stations
+    /api/v1.0/tobs
+    /api/v1.0/temp/start/end
+    ''')
